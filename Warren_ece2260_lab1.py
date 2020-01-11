a75ae38649a2c7a48d2ad220494bdc2ba04d4693
thomaswarren99 <thomaswarren@mail.weber.edu>
2020-01-10 22:42:00 -0700

    completed up to trapezoid riemann

--- Warren_ece2260_lab1.py
+++ Warren_ece2260_lab1.py
@@ -1,18 +1,20 @@
 import cmath
 import math
 
+# Calculates roots of coefficients using quadratic formula
 def calculate_roots(coef):
     a = coef[0]
-    b = coef[1]
+    b = coef[1]     #variables for the quadratic function
     c = coef[2]
 
     
-    p = (-b + cmath.sqrt((b**2) - 4 * a * c))/(2 * a)
+    p = (-b + cmath.sqrt((b**2) - 4 * a * c))/(2 * a)    #pos and neg roots
     q = (-b - cmath.sqrt((b**2) - 4 * a * c))/(2 * a)
     
     roots = (p,q)
     return roots
 
+# Calculates factorial of input n
 def compute_factorial(n):
     b = 1
     for ii in range(1, n + 1):
@@ -20,17 +22,20 @@ def compute_factorial(n):
     return b
     
     
-
+# sums up factorials using the compute_factorial for each input n
 def sum_factorial(n):
     a = 0
     for i in range(1, n+1):
         a += compute_factorial(i)
     return a
 
+# the formula of the function used on the y-axis in the riemann sums
 def f_x(x):
     a = (math.exp(-3*(x))) * math.cos((math.pi) * (x))
     return a
 
+# function used to calculate the left riemann sum with 
+# three inputs delta_x, lb, ub
 def left_riemann(delta_x, lb, ub):
     # to do
     d = int(ub/delta_x)
@@ -41,6 +46,8 @@ def left_riemann(delta_x, lb, ub):
         su += a
     return su
 
+# function used to calculate the right riemann sum with 
+# three inputs delta_x, lb, ub
 def right_riemann(delta_x, lb, ub):
     d = int(ub/delta_x)
     su = 0
@@ -51,13 +58,29 @@ def right_riemann(delta_x, lb, ub):
     return su
 
     
+# function finds the riemann sum by using the midpoint method with 
+# inputs delta_x, lb, ub
+def midpoint_riemann(delta_x, lb, ub):
+    d = int(ub/delta_x)
+    su = 0
+    for i in range(lb, d):
+        inc = delta_x * i
+        mid = (inc + (inc + delta_x))/2
+        a = delta_x * f_x(mid)
+        su += a
+    return su
 
-# def midpoint_riemann(delta_x, lb, ub):
-#     # to do
 
-    
-# def trap_riemann(delta_x, lb, ub):
-#     # to do
+# function finds the riemann sum by using the trapezoid method with 
+# inputs delta_x, lb, ub    
+def trap_riemann(delta_x, lb, ub):
+    d = int(ub/delta_x)
+    su = 0
+    for i in range(lb, d):
+        inc = i * delta_x
+        a = delta_x * ((f_x(inc) + f_x(inc + delta_x))/2)
+        su += a
+    return su
 
     
 def main():
@@ -118,25 +141,25 @@ def main():
     lb = 0
     ub = 10
     
-    print("calculating left Riemann sum")
-    for delta_x in [1, 0.1, 0.01, 0.0001]:
-        summation = left_riemann(delta_x, lb, ub)
-        print("\tdelta_x=%f, summation=%f" % (delta_x, summation))
+    # print("calculating left Riemann sum")
+    # for delta_x in [1, 0.1, 0.01, 0.0001]:
+    #     summation = left_riemann(delta_x, lb, ub)
+    #     print("\tdelta_x=%f, summation=%f" % (delta_x, summation))
 
-    print("calculating right Riemann sum")
-    for delta_x in [1, 0.1, 0.01, 0.0001]:
-        summation = right_riemann(delta_x, lb, ub)
-        print("\tdelta_x=%f, summation=%f" % (delta_x, summation))
+    # print("calculating right Riemann sum")
+    # for delta_x in [1, 0.1, 0.01, 0.0001]:
+    #     summation = right_riemann(delta_x, lb, ub)
+    #     print("\tdelta_x=%f, summation=%f" % (delta_x, summation))
 
     # print("calculating midpoint Riemann sum")
     # for delta_x in [1, 0.1, 0.01, 0.0001]:
     #     summation = midpoint_riemann(delta_x, lb, ub)
     #     print("\tdelta_x=%f, summation=%f" % (delta_x, summation))
 
-    # print("calculating trapezoid Riemann sum")
-    # for delta_x in [1, 0.1, 0.01, 0.0001]:
-    #     summation = trap_riemann(delta_x, lb, ub)
-    #     print("\tdelta_x=%f, summation=%f" % (delta_x, summation))
+    print("calculating trapezoid Riemann sum")
+    for delta_x in [1, 0.1, 0.01, 0.0001]:
+        summation = trap_riemann(delta_x, lb, ub)
+        print("\tdelta_x=%f, summation=%f" % (delta_x, summation))
 
         
 if __name__ == "__main__":
