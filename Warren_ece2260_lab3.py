8bb1e1812984b51d59cbd1b1fc21535ba9fc01e3
thomaswarren99 <54598629+thomaswarren99@users.noreply.github.com>
2020-01-22 20:23:30 -0700

    Add files via upload

--- Warren_ece2260_lab3.py
+++ Warren_ece2260_lab3.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#estimates time constant tau by comparing to known voltage value
 def find_time_constant(t, f_t):
     b = 0
     for ii in range(0, 10000):
@@ -9,11 +9,22 @@ def find_time_constant(t, f_t):
             b = ii
     return t[b]
 
+#plots the 
+def plot():
+    t = np.linspace(0, 0.005, 1000)
+    f_t = 5 - 5*np.exp(-1000*t)
+    plt.plot(t, f_t)
+    plt.xlabel("Time (ms)")
+    plt.ylabel("Voltage (V)")
+    plt.show()
+
 def main():
     t = np.linspace(0, 1, 100000)
     f_t = 5 - 5*np.exp(-1000*t)
     print(find_time_constant(t, f_t))
+    plot()
 
+    
 
 if __name__ == "__main__": 
     main()
